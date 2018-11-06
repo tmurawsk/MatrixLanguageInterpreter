88918abe35b7b6458f6796b2d1d33e63af8bf2c3
tmurawsk <tomasz.murawski313@gmail.com>
2018-11-06 21:53:43 +0100

    added file input reader

--- src/tkom/InputReader.java
+++ src/tkom/InputReader.java
@@ -1,4 +1,58 @@
 package tkom;
 
+import java.io.IOException;
+import java.io.InputStream;
+
 public class InputReader {
+    private InputStream stream;
+    private Position position;
+    private char peekedChar;
+    private boolean isCharPeeked;
+
+    public InputReader(InputStream stream) {
+        this.stream = stream;
+        position = new Position();
+        isCharPeeked = false;
+    }
+
+    public Position getPosition() {
+        return position;
+    }
+
+    public char read() throws IOException {
+        if(isEof())
+            throw new IOException();
+
+        char nextChar;
+        if(isCharPeeked) {
+            nextChar = peekedChar;
+            isCharPeeked = false;
+        } else {
+            nextChar = (char) stream.read();
+        }
+
+        position.charNum++;
+        if(nextChar == '\n') {
+            position.charNum = 1;
+            position.lineNum++;
+        }
+
+        return nextChar;
+    }
+
+    public char peek() throws IOException {
+        if(isEof())
+            throw new IOException();
+
+        if(!isCharPeeked) {
+            peekedChar = (char) stream.read();
+            isCharPeeked = true;
+        }
+
+        return peekedChar;
+    }
+
+    public boolean isEof() throws IOException {
+        return isCharPeeked || stream.available() > 0;
+    }
 }

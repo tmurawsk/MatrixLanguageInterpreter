772b1ae74ae535cb1cb14962d7a0daa31b12ea17
tmurawsk <tomasz.murawski313@gmail.com>
2019-01-12 13:54:37 +0100

    added "abstract" to class definition

--- src/tkom/exception/ParseException.java
+++ src/tkom/exception/ParseException.java
@@ -2,7 +2,7 @@ package tkom.exception;
 
 import tkom.Position;
 
-public class ParseException extends Exception {
+public abstract class ParseException extends Exception {
     ParseException(Position position, String message) {
         super("In line: " + position.lineNum + ", " + position.charNum + ":\n\t"
                 + "ERROR: " + message);
